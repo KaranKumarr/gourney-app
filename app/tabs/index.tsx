5677ba21ed06ed15d2c6fc8373561a2e2565b675
karankumarr <karankumar0334@gmail.com>
2024-10-14 13:56:29 +0500

    floating action button

--- app/tabs/index.tsx
+++ app/tabs/index.tsx
@@ -1,4 +1,10 @@
-import { View, Text, TouchableOpacity, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ScrollView,
+} from "react-native";
 import React from "react";
 import { colors, spacing, textStyles } from "@/constants/theme";
 import TopBar from "@/components/core/TopBar";
@@ -9,118 +15,141 @@ import StressedEmoji from "@/assets/emoji-stressed.svg";
 import CalmEmoji from "@/assets/emoji-calm.svg";
 import AngryEmoji from "@/assets/emoji-angry.svg";
 import dayjs from "dayjs";
+import { Plus } from "lucide-react-native";
 
 const Home = () => {
   const { journalEntries } = useJournalEntriesStore();
 
   return (
     <View style={{ flex: 1, backgroundColor: colors.backgroundLight }}>
-      <TopBar />
-      <View style={{ paddingHorizontal: spacing.medium, gap: spacing.medium }}>
-        <View>
-          <Text
-            style={[textStyles.heading, { fontFamily: "Roboto_400Regular" }]}
-          >
-            Good Morning!
-          </Text>
-          <Text style={[textStyles.heading]}>Karchna</Text>
-        </View>
+      <ScrollView style={{ flex: 1, backgroundColor: colors.backgroundLight }}>
+        <TopBar />
         <View
           style={{
-            backgroundColor: colors.cardLight,
-            padding: spacing.medium,
-            paddingBottom: spacing.small,
-            borderRadius: 4,
-            gap: spacing.small,
-            alignItems: "center",
+            paddingHorizontal: spacing.medium,
+            gap: spacing.medium,
           }}
         >
-          <Text style={[textStyles.subheading]}>
-            How are you feeling today?
-          </Text>
+          <View>
+            <Text
+              style={[textStyles.heading, { fontFamily: "Roboto_400Regular" }]}
+            >
+              Good Morning!
+            </Text>
+            <Text style={[textStyles.heading]}>Karchna</Text>
+          </View>
+          <View
+            style={{
+              backgroundColor: colors.cardLight,
+              padding: spacing.medium,
+              paddingBottom: spacing.small,
+              borderRadius: 4,
+              gap: spacing.small,
+              alignItems: "center",
+            }}
+          >
+            <Text style={[textStyles.subheading]}>
+              How are you feeling today?
+            </Text>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "center",
+              }}
+            >
+              <TouchableOpacity style={{ padding: spacing.small }}>
+                <HappyEmoji height={24} width={24} />
+              </TouchableOpacity>
+
+              <TouchableOpacity style={{ padding: spacing.small }}>
+                <CalmEmoji height={24} width={24} />
+              </TouchableOpacity>
+
+              <TouchableOpacity style={{ padding: spacing.small }}>
+                <SadEmoji height={24} width={24} />
+              </TouchableOpacity>
+
+              <TouchableOpacity style={{ padding: spacing.small }}>
+                <StressedEmoji height={24} width={24} />
+              </TouchableOpacity>
+
+              <TouchableOpacity style={{ padding: spacing.small }}>
+                <AngryEmoji height={24} width={24} />
+              </TouchableOpacity>
+            </View>
+          </View>
           <View
             style={{
               flexDirection: "row",
-              justifyContent: "center",
+              justifyContent: "space-between",
+              alignItems: "center",
             }}
           >
-            <TouchableOpacity style={{ padding: spacing.small }}>
-              <HappyEmoji height={24} width={24} />
-            </TouchableOpacity>
+            <Text style={[textStyles.subheading]}>Recent Entries</Text>
 
-            <TouchableOpacity style={{ padding: spacing.small }}>
-              <CalmEmoji height={24} width={24} />
-            </TouchableOpacity>
-
-            <TouchableOpacity style={{ padding: spacing.small }}>
-              <SadEmoji height={24} width={24} />
-            </TouchableOpacity>
-
-            <TouchableOpacity style={{ padding: spacing.small }}>
-              <StressedEmoji height={24} width={24} />
-            </TouchableOpacity>
-
-            <TouchableOpacity style={{ padding: spacing.small }}>
-              <AngryEmoji height={24} width={24} />
+            <TouchableOpacity>
+              <Text style={[textStyles.body, { color: colors.primary }]}>
+                View All
+              </Text>
             </TouchableOpacity>
           </View>
-        </View>
-        <View
-          style={{
-            flexDirection: "row",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <Text style={[textStyles.subheading]}>Recent Entries</Text>
+          <FlatList
+            data={journalEntries}
+            renderItem={(entry) => {
+              return (
+                <View
+                  style={{
+                    borderWidth: 0.5,
+                    borderColor: colors.neutralLight,
+                    padding: spacing.medium,
+                    borderRadius: 4,
+                    gap: spacing.small / 2,
+                    marginBottom: spacing.medium,
+                  }}
+                >
+                  <View style={{ flexDirection: "row" }}>
+                    <Text
+                      style={[
+                        textStyles.smallText,
+                        { fontFamily: "Roboto_500Medium" },
+                      ]}
+                    >
+                      {dayjs(entry.item.entryDateTime).format("hh:mm A")}
+                    </Text>
+                    <Text style={[textStyles.smallText]}>
+                      {dayjs(entry.item.entryDateTime).format("  - D MMM YYYY")}
+                    </Text>
+                  </View>
 
-          <TouchableOpacity>
-            <Text style={[textStyles.body, { color: colors.primary }]}>
-              View All
-            </Text>
-          </TouchableOpacity>
-        </View>
-        <FlatList
-          data={journalEntries}
-          renderItem={(entry) => {
-            return (
-              <View
-                style={{
-                  borderWidth: 0.5,
-                  borderColor: colors.neutralLight,
-                  padding: spacing.medium,
-                  borderRadius: 4,
-                  gap: spacing.small / 2,
-                  marginBottom: spacing.medium,
-                }}
-              >
-                <View style={{ flexDirection: "row" }}>
+                  <Text style={[textStyles.label]}>{entry.item.title}</Text>
                   <Text
-                    style={[
-                      textStyles.smallText,
-                      { fontFamily: "Roboto_500Medium" },
-                    ]}
+                    style={[textStyles.body, { color: colors.neutralLight }]}
+                    numberOfLines={3}
                   >
-                    {dayjs(entry.item.entryDateTime).format("hh:mm A")}
-                  </Text>
-                  <Text style={[textStyles.smallText]}>
-                    {dayjs(entry.item.entryDateTime).format("  - D MMM YYYY")}
+                    {entry.item.body.trim()}
                   </Text>
                 </View>
-
-                <Text style={[textStyles.label]}>{entry.item.title}</Text>
-                <Text
-                  style={[textStyles.body, { color: colors.neutralLight }]}
-                  numberOfLines={3}
-                >
-                  {entry.item.body.trim()}
-                </Text>
-              </View>
-            );
-          }}
-          keyExtractor={(entry) => entry.id.toString()}
-        />
-      </View>
+              );
+            }}
+            keyExtractor={(entry) => entry.id.toString()}
+          />
+        </View>
+      </ScrollView>
+      <TouchableOpacity
+        style={{
+          position: "absolute",
+          bottom: spacing.large,
+          right: 0,
+          backgroundColor: colors.primary,
+          zIndex: 10,
+          marginHorizontal: spacing.medium,
+          padding: spacing.small,
+          borderRadius: 1000,
+          elevation: 2,
+        }}
+      >
+        <Plus color={colors.backgroundLight} height={24} width={24} />
+      </TouchableOpacity>
     </View>
   );
 };
