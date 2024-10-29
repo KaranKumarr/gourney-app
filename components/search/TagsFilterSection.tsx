d5b3955a0f8995082cbcd9362fbe44045378a536
karankumarr <karankumar0334@gmail.com>
2024-10-30 01:16:14 +0500

    tags filter section skeleton

---
+++ components/search/TagsFilterSection.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+// @TODO TAGS FILTER SECTION
+const TagsFilterSection = () => {
+  return (
+    <View>
+      <Text>TagsFilterSection</Text>
+    </View>
+  );
+};
+
+export default TagsFilterSection;
